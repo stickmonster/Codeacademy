32a550b562cdc385409096259a7c779421841e8b
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2021-02-05 18:37:41 +0000

    Add files via upload

---
+++ Codecademy Block Letters/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << " TTTTT\n";
+	std::cout << "   T  \n";
+	std::cout << "   T  \n";
+	std::cout << "   T  \n";
+	std::cout << "   T  \n\n";
+	std::cout << "  OOO \n";
+	std::cout << "O     O\n";
+	std::cout << "O     O\n";
+	std::cout << "O     O\n";
+	std::cout << "  OOO \n\n";
+	std::cout << "BBBB\n";
+	std::cout << "B   B\n";
+	std::cout << "BBBBB\n";
+	std::cout << "B   B\n";
+	std::cout << "BBBB\n\n";
+	std::cout << "Y   Y\n";
+	std::cout << " Y Y\n";
+	std::cout << "  Y  \n";
+	std::cout << "  Y  \n";
+	std::cout << "  Y  \n";
+
+	return 0;
+
+}
